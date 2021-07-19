3e61993f94a99df2a34407359983330c57a96828
Jules Fouchy <jules.fouchy@ntymail.com>
2021-07-19 21:35:16 +0200

    Update Cool

--- src/App.h
+++ src/App.h
@@ -1,48 +1,46 @@
 #pragma once
 
 #include <Cool/App/IApp.h>
+#include <Cool/App/Window.h>
 #include <Cool/OpenGL/Shader.h>
 #include <Cool/Renderer_Fullscreen/Renderer_Fullscreen.h>
-#include <Cool/App/OpenGLWindow.h>
-
 #include "Serialization/SerializedClassExample.h"
 
 using namespace Cool;
 
 class App : public Cool::IApp {
 public:
-	App(OpenGLWindow& mainWindow);
-	~App();
+    App(Window& mainWindow);
+    ~App();
 
-	void update() override;
-	void ImGuiWindows() override;
-	void ImGuiMenus() override;
+    void update() override;
+    void ImGuiWindows() override;
+    void ImGuiMenus() override;
 
-	void onKeyboardEvent(int key, int scancode, int action, int mods) override;
-	void onMouseButtonEvent(int button, int action, int mods) override;
-	void onScrollEvent(double xOffset, double yOffset) override;
-	void onMouseMoveEvent(double xPos, double yPos) override;
+    void onKeyboardEvent(int key, int scancode, int action, int mods) override;
+    void onMouseButtonEvent(int button, int action, int mods) override;
+    void onScrollEvent(double xOffset, double yOffset) override;
+    void onMouseMoveEvent(double xPos, double yPos) override;
 
 private:
-	OpenGLWindow& m_mainWindow;
-	Renderer_Fullscreen m_renderer;
-	Shader m_shader;
-	glm::vec3 m_bgColor = glm::vec3(0.478f, 0.674f, 0.792f);
-	SerializedClassExample m_serializedClassExample;
+    Window&                m_mainWindow;
+    Renderer_Fullscreen    m_renderer;
+    Shader                 m_shader;
+    glm::vec3              m_bgColor = glm::vec3(0.478f, 0.674f, 0.792f);
+    SerializedClassExample m_serializedClassExample;
 #ifndef NDEBUG
-	bool m_bShow_Debug = true;
-	bool m_bShow_ImGuiDemo = false;
+    bool m_bShow_Debug     = true;
+    bool m_bShow_ImGuiDemo = false;
 #endif
 
 private:
-	//Serialization
-	friend class cereal::access;
-	template<class Archive>
-	void serialize(Archive& archive)
-	{
-		archive(
-			cereal::make_nvp("A serialization example", m_serializedClassExample),
-			CEREAL_NVP(m_bgColor)
-		);
-	}
+    // Serialization
+    friend class cereal::access;
+    template<class Archive>
+    void serialize(Archive& archive)
+    {
+        archive(
+            cereal::make_nvp("A serialization example", m_serializedClassExample),
+            CEREAL_NVP(m_bgColor));
+    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,57 @@
-#include <Cool/Log/Log.h>
-#include <Cool/App/AppManager.h>
-#include <Cool/App/OpenGLWindowingSystem.h>
+#ifdef DEBUG
+    #define DOCTEST_CONFIG_IMPLEMENT
+    #include <doctest/doctest.h>
+#endif
 
+#include <Cool/App/AppManager.h>
+#include <Cool/App/WindowFactory.h>
+#include <Cool/File/File.h>
+#include <Cool/Log/Log.h>
+#include <Cool/Time/Time.h>
 #include "App.h"
 
-int main() {
-	// Init Logging
-	Cool::Log::Initialize();
-	// Init Glfw and set OpenGL version to 4.3
-	Cool::OpenGLWindowingSystem windowingSystem(4, 3);
-	// Create the main window and its OpenGL context
-	Cool::OpenGLWindow mainWindow = windowingSystem.createWindow("You can change the window name in main.cpp", 1280, 720);
-#ifdef NDEBUG
-	glfwMaximizeWindow(mainWindow.get());
+int main(int argc, char** argv)
+{
+#ifdef DEBUG
+    // Run the tests !
+    doctest::Context context;
+    context.applyCommandLine(argc, argv);
+    int res = context.run();
+    if (context.shouldExit()) {
+        return res;
+    }
 #endif
-	// App
-	App app(mainWindow);
-	Cool::AppManager appManager(mainWindow, app);
-	appManager.run();
+
+    // Init
+    Cool::Log::initialize();
+#ifdef DEBUG
+    // When launching from an IDE (which almost always means we are in Debug mode)
+    // the initial current_path will be set to bin/Debug
+    // but I prefer to have it at the root of the project, so that the assets we load
+    // are the ones at the root of the project, not the ones that have been copied to the bin folders.
+    // This means that if for example we do hot reloading of shaders
+    // we can modify the source assets (at the root), instead of the ones that have been copied.
+    // This is important because only the ones at the root are tracked in Git.
+    // Also, if you modify an asset at the root whithout modifying some code, and then restart the program
+    // the asset won't be copied (because I couldn't figure out how to configure CMake to do so)
+    // So it is better to always work with the assets at the root while in development.
+    std::filesystem::current_path(CMAKE_SOURCE_DIR);
+#endif
+    Cool::File::initialize_root_dir(std::filesystem::current_path().string());
+    Cool::Time::initialize();
+    // Init Glfw and set OpenGL version
+#ifndef __APPLE__
+    Cool::WindowFactory window_factory(4, 3);
+#else
+    Cool::WindowFactory window_factory(3, 3); // OpenGL > 3.3 is not supported on MacOS
+#endif
+    // Create the main window and init OpenGL
+    Cool::Window& mainWindow = window_factory.create("You can change the window name in main.cpp", 1280, 720);
+#ifndef DEBUG
+    glfwMaximizeWindow(mainWindow.get());
+#endif
+    // App
+    App              app(mainWindow);
+    Cool::AppManager appManager(mainWindow, app);
+    appManager.run();
 }
