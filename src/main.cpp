763047ee44117a0090bba86bb6547f8974899783
Jules Fouchy <jules.fouchy@ntymail.com>
2021-08-13 19:35:35 +0200

    Update Cool

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,12 @@ void main()
 {
     Cool::initialize();
     Cool::WindowFactory window_factory;
-    Cool::Window&       window = window_factory.make_window("You can change the window name in main.cpp", 1280, 720);
+    Cool::Window& window = window_factory.make_main_window("You can change the window name in main.cpp", 1280, 720);
 #if !defined(DEBUG)
     glfwMaximizeWindow(window->glfw());
 #endif
     // App
     App              app(window);
-    Cool::AppManager appManager(window, app);
+    Cool::AppManager appManager(window, window_factory.window_manager(), app);
     appManager.run();
 }
