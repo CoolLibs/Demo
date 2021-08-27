69bda92c3e405ee61bc8dc5da0b72cfbdf196595
Jules Fouchy <jules.fouchy@ntymail.com>
2021-08-27 18:32:39 +0200

    Cool : add a shut_down()

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include <Cool/App/AppManager.h>
-#include <Cool/Core/initialize.h>
+#include <Cool/Core/initialize_and_shutdown.h>
 #include <Cool/Window/WindowFactory.h>
 #include "App.h"
 
 void main()
 {
     Cool::initialize();
-    Cool::WindowFactory window_factory(4, 3);
-    Cool::Window&       window = window_factory.make_main_window("You can change the window name in main.cpp", 1280, 720);
+    auto  window_factory = Cool::WindowFactory{4, 3};
+    auto& window         = window_factory.make_main_window("You can change the window name in main.cpp", 1280, 720);
 #if !defined(DEBUG)
     glfwMaximizeWindow(window.glfw());
 #endif
@@ -15,4 +15,5 @@ void main()
     App              app(window);
     Cool::AppManager appManager(window, window_factory.window_manager(), app);
     appManager.run();
+    Cool::shut_down();
 }
