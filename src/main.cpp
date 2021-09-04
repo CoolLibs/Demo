e472651026cdf935ddda4ec0d93b178749d0c199
Jules Fouchy <jules.fouchy@ntymail.com>
2021-09-05 00:05:15 +0200

    Set OpenGL version from CMake

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,13 @@
 void main()
 {
     Cool::initialize();
-#if defined(__COOL_APP_OPENGL)
-    auto window_factory = Cool::WindowFactory{4, 3};
-#elif defined(__COOL_APP_VULKAN)
-    auto window_factory = Cool::WindowFactory{};
-#endif
-    auto& window = window_factory.make_main_window("You can change the window name in main.cpp", 1280, 720);
+    auto  window_factory = Cool::WindowFactory{};
+    auto& window         = window_factory.make_main_window({"You can change the window name in main.cpp"});
 #if !defined(DEBUG)
     glfwMaximizeWindow(window.glfw());
 #endif
-    // App
-    App              app(window);
-    Cool::AppManager appManager(window, window_factory.window_manager(), app);
-    appManager.run();
+    auto app         = App{window};
+    auto app_manager = Cool::AppManager{window, window_factory.window_manager(), app};
+    app_manager.run();
     Cool::shut_down();
 }
