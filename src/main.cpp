944db867b31da51265d44cc5c2ff48dc487228ce
Jules Fouchy <jules.fouchy@ntymail.com>
2021-11-16 16:10:57 +0100

    [Cool] Provide an InitConfig struct instead of one monolithic DefaultInit function

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 #include <Cool/Core/run.h>
-#include <Cool/Default/DefaultInit.h>
 #include "App.h"
 
 int main()
 {
-    CoolDefault::init();
     Cool::run<App>({Cool::WindowConfig{.title = "You can change the window title in main.cpp"}},
+                   Cool::InitConfig{.log_pattern = "%^[%e] %n: %v%$"},
                    Cool::AppManagerConfig{.dispatch_keyboard_events_to_imgui = true});
 }
